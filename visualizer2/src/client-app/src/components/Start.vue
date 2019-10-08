7201ac2eb15ce00b790b03d9b7a59b8467fe14d9
Johan Hellgren <hellgrenj@gmail.com>
2019-10-08 10:29:21 +0200

    save commit; wiring up some UI events in vue

--- visualizer2/src/client-app/src/components/Start.vue
+++ visualizer2/src/client-app/src/components/Start.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="main">
     <div id="choices">
+      <div>
       <span style="font-size:0.8em;color:#909090;">välj ett nyckelord</span>
       <br />
       <span v-for="keyword in keywords" :key="keyword">
-        
         <span v-if="keyword == currentKeyword" class="selectedKeywordOption">{{ keyword }}</span>
-        <span v-else class="keywordOption">{{ keyword }}</span>
+        <span v-else class="keywordOption" v-on:click="selectKeyword">{{ keyword }}</span>
       </span>
-    </div>
-    <div>
-      <ul class="yearMonths" v-for="yearMonth in yearMonths" :key="yearMonth">
-        <li class="yearMonth">{{ yearMonth }}</li>
-      </ul>
+      </div>
+      <div>
+      <span style="font-size:0.8em;color:#909090;">välj positivt eller negativt</span>
+      <br />
+      <span v-for="yearMonth in yearMonths" :key="yearMonth">
+        <span v-if="yearMonth == currentYearMonth" class="selectedYearMonthOption">{{ yearMonth }}</span>
+        <span v-else class="yearMonthOption" v-on:click="selectYearMonth">{{ yearMonth }}</span>
+      </span>
+      </div>
     </div>
   </div>
 </template>
@@ -24,8 +28,17 @@ export default {
     return {
       keywords: [],
       yearMonths: [],
-      currentKeyword: 'Moderaterna'
+      currentKeyword: "",
+      currentYearMonth: ""
     };
+  },
+  methods: {
+    selectKeyword: function(event) {
+      this.currentKeyword = event.target.innerHTML;
+    },
+    selectYearMonth: function(event) {
+      this.currentYearMonth = event.target.innerHTML;
+    }
   },
   mounted() {
     console.log("test");
@@ -35,6 +48,7 @@ export default {
         let keywords = res.data;
         console.log(keywords);
         this.keywords = keywords;
+        this.currentKeyword = keywords[0];
       })
       .catch(err => {
         console.log(err);
@@ -45,6 +59,7 @@ export default {
         let yearMonths = res.data;
         console.log(yearMonths);
         this.yearMonths = yearMonths;
+        this.currentYearMonth = yearMonths[0];
       })
       .catch(err => {
         console.log(err);
